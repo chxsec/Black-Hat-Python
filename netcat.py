ac6ee08ca2b23ddf3563306f2de0671c819ba3b1
chxsec <flip3604bl@protonmail.com>
2024-12-11 21:30:49 -0800

    fix type

--- netcat.py
+++ netcat.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
         echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # connect to server port 135
         netcat.py -t 192.168.192.108 -p 5555 # connect to server
     '''))
-parser.add_argument('c', '--command', action='store_true', help='command shell')
+parser.add_argument('-c', '--command', action='store_true', help='command shell')
 parser.add_argument('-e', '--execute', help='execute specified command')
-parser.add_argument('-l' '--listen', action='store_true', help='listen')
+parser.add_argument('-l', '--listen', action='store_true', help='listen')
 parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
 parser.add_argument('-t', '--target', default='192.168.192.203', help='specified IP')
 parser.add_argument('-u', '--upload', help='upload file')
