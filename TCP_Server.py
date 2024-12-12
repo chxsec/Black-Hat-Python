99d0521a9ea13bac2713db9e6c167c75b08a3d20
chxsec <flip3604bl@protonmail.com>
2024-12-11 20:29:41 -0800

    add missing ACK

--- TCP_Server.py
+++ TCP_Server.py
@@ -20,6 +20,7 @@ def handle_client(client_socket):
     with client_socket as sock:
         request = sock.recv(1024)
         print(f'[*] Recieved: {request.decode("utf-8")}')
+        sock.send(b'ACK')
 
 if __name__ == '__main__':
     main()
