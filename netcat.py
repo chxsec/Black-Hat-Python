61f4baa5c1fc8f4547716c90fbaaf6fa7faa1d22
chxsec <flip3604bl@protonmail.com>
2025-01-04 21:19:17 -0800

    adding netcat code

--- netcat.py
+++ netcat.py
@@ -39,3 +39,39 @@ else:
 
 nc = NetCat(args, buffer.encode())
 nc.run()
+
+class NetCat:
+    def __init__(self, args, buffer=none):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF__INET, socket.sock_stream)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def run(self):
+        if self.args.listen
+            self.listen()
+        else:
+            self.send()
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminsted.')
+            self.socket.close()
+            sys.exit()
